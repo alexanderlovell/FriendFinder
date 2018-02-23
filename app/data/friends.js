b8bb72deabf7375314869634ddb5f1d5b071d39c
Alex <alovell@salesforce.com>
2018-02-22 16:02:49 -0800

    updated repo

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+// seed database
+
+var friends = [ {
+	"name" : "Monica",
+	"photo" : "http://imageinlife-application.eu/img/177b516b1fd340da04d5a3821bdd7e8f.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Simon",
+	"photo" : "http://cdn.smosh.com/sites/default/files/2015/03/funny-tinder-profile-chicks.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Rhea",
+	"photo" : "https://strongerpicture.com/blog/wp-content/uploads/2017/10/xi_tinder05.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
