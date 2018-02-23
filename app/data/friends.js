a029c9993b4160578a756fdff8c1cdaabb9860aa
Alex <alovell@salesforce.com>
2018-02-22 16:20:20 -0800

    updated friends seeding

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // seed database
 
 var friends = [ {
-	"name" : "Monica",
+	"name" : "Stefanie",
 	"photo" : "http://imageinlife-application.eu/img/177b516b1fd340da04d5a3821bdd7e8f.jpg",
 	"scores" : [5,5,2,3,1,5,2,1,3,5]
 }, {
@@ -9,7 +9,7 @@ var friends = [ {
 	"photo" : "http://cdn.smosh.com/sites/default/files/2015/03/funny-tinder-profile-chicks.jpg",
 	"scores" : [1,5,1,1,1,1,1,1,5,5]
 }, {
-	"name" : "Rhea",
+	"name" : "Bethany",
 	"photo" : "https://strongerpicture.com/blog/wp-content/uploads/2017/10/xi_tinder05.jpg",
 	"scores" : [5,3,2,3,2,5,1,3,3,4]
 }];
